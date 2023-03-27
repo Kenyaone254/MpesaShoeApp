4d1d634ade0929190362a0a52e1e076fff4eb5e3
kenyaone254 <ivyn8384@gmail.com>
2023-03-27 13:59:54 +0300

    created Mpesa Shoe app

---
+++ ShoeApp/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Shoe
+from .models import Checkout
+
+
+# Register your models here.
+class ShoeAdmin(admin.ModelAdmin):
+    list_display = ('shoename', 'description', 'price', 'imageurl')
+
+
+class CheckoutAdmin(admin.ModelAdmin):
+    list_display = ('transactionid', 'amount', 'phone')
+
+
+admin.site.register(Shoe, ShoeAdmin)
+admin.site.register(Checkout, CheckoutAdmin)
+from django.contrib import admin
+
+# Register your models here.
